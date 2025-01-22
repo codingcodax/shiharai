71c65a8bab68a0385b694397ea5376b57e3b29e7
Alexis Guzman <codingcodax@gmail.com>
2025-01-21 20:57:30 -0600

    feat(api): create `auth.sign-out` method

---
+++ src/server/api/routers/auth/sign-out.ts
@@ -0,0 +1,10 @@
+import { invalidateSessionToken } from "~/server/auth";
+import { protectedProcedure } from "../../trpc";
+
+export const signOut = protectedProcedure.mutation(async (opts) => {
+  if (!opts.ctx.token) return { success: false };
+
+  await invalidateSessionToken(opts.ctx.token);
+
+  return { success: true };
+});
