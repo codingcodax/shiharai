ff9d373d42881d8932f620675bb418664c89404c
Alexis Guzman <codingcodax@gmail.com>
2025-01-21 20:57:47 -0600

    feat(api): create auth router

---
+++ src/server/api/routers/auth/auth.ts
@@ -0,0 +1,9 @@
+import type { TRPCRouterRecord } from "@trpc/server";
+
+import { getSession } from "./get-session";
+import { signOut } from "./sign-out";
+
+export const authRouter = {
+  signOut,
+  getSession,
+} satisfies TRPCRouterRecord;
