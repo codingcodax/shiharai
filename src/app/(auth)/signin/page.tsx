e710ef2f5538b32f4c6ae0d187d71437b3ddf3d5
Alexis Guzman <codingcodax@gmail.com>
2025-01-26 12:31:09 -0600

    feat(pages): setup sign in

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from '~/components/ui/button';
+import { signIn } from '~/server/auth';
+
 const Page = () => {
+  const handleSignIn = async () => {
+    'use server';
+    await signIn('github', { redirect: true, redirectTo: '/dashboard' });
+  };
+
   return (
-    <div>
-      <p>sign in page</p>
+    <div className='grid h-screen w-full grid-rows-2 gap-8 p-4'>
+      <div className='h-full rounded-3xl bg-grey-bg' />
+
+      <div className='flex w-full flex-col justify-between gap-4'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-bold'>Sign in</h1>
+          <p className='mt-2 text-xl text-grey-text'>
+            Manage your subscription in an efficient way
+          </p>
+        </div>
+
+        <Button className='w-full' onClick={handleSignIn}>
+          Sign in with GitHub
+        </Button>
+      </div>
     </div>
   );
 };
