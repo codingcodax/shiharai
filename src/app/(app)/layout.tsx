ac554352f82e3a2d266f8e2882a7658bb89a5417
Alexis Guzman <codingcodax@gmail.com>
2025-01-26 12:32:52 -0600

    feat(layout): create for route group `(app)`

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from './_components/navbar';
+
+const Layout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <>
+      {children}
+      <Navbar />
+    </>
+  );
+};
+
+export default Layout;
