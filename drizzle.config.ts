03298de305f1fa22d7783bb61f2a21af002b0f15
Alexis Guzman <codingcodax@gmail.com>
2025-02-02 17:20:17 -0600

    feat(drizzle): use no pooling url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { type Config } from 'drizzle-kit';
 
 import { env } from '~/env';
 
+const nonPoolingUrl = env.POSTGRES_URL.replace(':6543', ':5432');
+
 export default {
   schema: './src/server/db/schema',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dbCredentials: { url: nonPoolingUrl },
   tablesFilter: ['shiharai_*'],
 } satisfies Config;
