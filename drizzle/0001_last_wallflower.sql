ca9c4da07d6ddf884daa190327fd6fc4c00d4067
Alexis Guzman <codingcodax@gmail.com>
2025-02-02 18:25:12 -0600

    feat(db): run `db:generate` script

---
+++ drizzle/0001_last_wallflower.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "shiharai_user" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
+ALTER TABLE "shiharai_category" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
+ALTER TABLE "shiharai_payment_method" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
+ALTER TABLE "shiharai_subscription" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
--- drizzle/0002_needy_hydra.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "shiharai_subscription" ALTER COLUMN "status" SET DATA TYPE shiharai_status;
