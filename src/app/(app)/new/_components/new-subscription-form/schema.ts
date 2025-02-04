8418e075a928615f1b7d4f433e1d4a5bffea0e21
Alexis Guzman <codingcodax@gmail.com>
2025-02-04 09:50:55 -0600

    feat(schemas): create details

--- src/app/(app)/new/_components/new-subscription-form/schema.ts
+++ src/app/(app)/new/_components/new-subscription-form/schema.ts
@@ -3,18 +3,31 @@ import { z } from 'zod';
 export type NewSubscription = z.infer<typeof NewSubscription>;
 
 export const NewSubscription = z.object({
+  // details
   logo: z.string(),
   name: z.string(),
   price: z.number(),
   currency: z.string(),
-  billingCycle: z.string(),
-  nextBillingDate: z.date(),
-  cancellationDate: z.date(),
   freeTrial: z.boolean(),
   trialDays: z.number(),
-  timezone: z.string(),
-  status: z.enum(['ACTIVE', 'PAUSED', 'CANCELLED', 'EXPIRED']),
   tier: z.string(),
-  url: z.string(),
   notes: z.string(),
+  url: z.string(),
+  // billing info
+  billingCycle: z.string(),
+  nextBillingDate: z.date(),
+  timezone: z.string(),
+});
+
+export type NewSubscriptionDetails = z.infer<typeof NewSubscriptionDetails>;
+export const NewSubscriptionDetails = NewSubscription.pick({
+  logo: true,
+  name: true,
+  price: true,
+  currency: true,
+  freeTrial: true,
+  trialDays: true,
+  tier: true,
+  notes: true,
+  url: true,
 });
