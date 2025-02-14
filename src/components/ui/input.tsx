8ab7b6f6776f18e46af8c815550d5283f0a7d5b8
Alexis Guzman <codingcodax@gmail.com>
2025-02-13 21:17:16 -0600

    feat(ui): update the z index when focus visible

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const inputStyles = tv({
     'flex h-9 w-full justify-center rounded-lg border border-grey-border bg-grey-base px-3 py-1 text-sm outline-none transition-colors duration-200',
     'placeholder:text-grey-placeholder-text',
     'hover:border-grey-border-hover',
-    'focus:ring-4 focus:ring-grey-focus-ring',
+    'focus:z-[1] focus:ring-4 focus:ring-grey-focus-ring',
     'disabled:cursor-not-allowed disabled:border-grey-line disabled:bg-grey-bg disabled:text-grey-solid',
     'aria-[invalid=true]:border-error-border',
     'aria-[invalid=true]:hover:border-error-border-hover',
