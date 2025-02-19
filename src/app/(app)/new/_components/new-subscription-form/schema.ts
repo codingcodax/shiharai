5428e6e1c634f8b1db295ccb32700ba31f9d6df4
Alexis Guzman <codingcodax@gmail.com>
2025-02-18 21:09:49 -0600

    feat(schemas): add error messages

--- src/app/(app)/new/_components/new-subscription-form/schema.ts
+++ src/app/(app)/new/_components/new-subscription-form/schema.ts
@@ -1,18 +1,36 @@
 import { z } from 'zod';
 
 export type NewSubscription = z.infer<typeof NewSubscription>;
-
 export const NewSubscription = z.object({
   // details
   logo: z.string(),
-  name: z.string(),
-  price: z.number(),
-  currency: z.string(),
+  name: z
+    .string({ message: 'Enter a name for your subscription' })
+    .min(2, 'Enter a name for your subscription'),
+  price: z
+    .number({ message: 'Enter a price for your subscription' })
+    .min(1, { message: 'Enter a price for your subscription' }),
+  currency: z.string({ message: 'Select a currency' }),
   freeTrial: z.boolean(),
   trialDays: z.number(),
   tier: z.string(),
   notes: z.string(),
-  url: z.string(),
+  url: z.string().refine(
+    (val) => {
+      if (val === '') {
+        return true; // Skip URL validation if empty
+      }
+      try {
+        new URL(val); // Basic URL check
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    {
+      message: 'Enter a valid URL',
+    },
+  ),
   // billing info
   billingCycle: z.string(),
   nextBillingDate: z.date(),
@@ -30,4 +48,15 @@ export const NewSubscriptionDetails = NewSubscription.pick({
   tier: true,
   notes: true,
   url: true,
-});
+}).refine(
+  (data) => {
+    if (data.freeTrial && data.trialDays < 1) {
+      return false;
+    }
+    return true;
+  },
+  {
+    message: 'Enter your free trial days',
+    path: ['trialDays'],
+  },
+);
