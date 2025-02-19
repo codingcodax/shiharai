dc66e4817994925dacd1b086e0997e7672c22b66
Alexis Guzman <codingcodax@gmail.com>
2025-02-18 21:09:28 -0600

    feat(layout): add next ssr plugin

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,13 @@
+import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin';
+import { extractRouterConfig } from 'uploadthing/server';
+
+import { ourFileRouter } from '~/app/api/uploadthing/core';
 import { Navbar } from './_components/navbar';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <>
+      <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       {children}
       <Navbar />
     </>
