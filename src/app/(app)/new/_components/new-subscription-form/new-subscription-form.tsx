74a93ad5a2a9bf054888571850d2b8bab9971630
Alexis Guzman <codingcodax@gmail.com>
2025-02-23 14:19:51 -0600

    feat(components): add billing info step

--- src/app/(app)/new/_components/new-subscription-form/new-subscription-form.tsx
+++ src/app/(app)/new/_components/new-subscription-form/new-subscription-form.tsx
@@ -1,14 +1,31 @@
 'use client';
 
+import { Progress } from '~/components/ui/progress';
+import { BillingInfoStep } from './billing-info-step';
 import { SubscriptionStep, useNewSubscriptionContext } from './context';
 import { DetailsStep } from './details-step';
 
 export const NewSubscriptionForm = () => {
-  const { currentStep } = useNewSubscriptionContext();
+  const { currentStep, steps } = useNewSubscriptionContext();
 
   return (
     <div className='space-y-6 p-4'>
+      <div className='space-y-1'>
+        <span>
+          Step {steps.indexOf(currentStep) + 1} of {steps.length}
+        </span>
+        <Progress
+          className='w-full'
+          value={Number(
+            (((steps.indexOf(currentStep) + 1) / steps.length) * 100).toFixed(
+              2,
+            ),
+          )}
+        />
+      </div>
+
       {currentStep === SubscriptionStep.DETAILS && <DetailsStep />}
+      {currentStep === SubscriptionStep.BILLING && <BillingInfoStep />}
     </div>
   );
 };
