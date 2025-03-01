f7f07274425ca3dc1d18cf05494b78b06bcdfaef
Alexis Guzman <codingcodax@gmail.com>
2025-02-28 20:33:52 -0600

    feat(api): add `get-all` method

--- src/server/api/routers/payment-method/payment-method.ts
+++ src/server/api/routers/payment-method/payment-method.ts
@@ -1,7 +1,10 @@
 import type { TRPCRouterRecord } from '@trpc/server';
 
 import { create } from './create';
+import { getAll } from './get-all';
 
 export const paymentMethodRouter = {
+  getAll,
+
   create,
 } satisfies TRPCRouterRecord;
