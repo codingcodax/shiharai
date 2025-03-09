a3d5e2c1934edefa680397d1f06f563ecc5850de
Alexis Guzman <codingcodax@gmail.com>
2025-03-09 12:39:48 -0600

    feat(api): add `get-all` method

--- src/server/api/routers/subscription/subscription.ts
+++ src/server/api/routers/subscription/subscription.ts
@@ -1,7 +1,10 @@
 import type { TRPCRouterRecord } from '@trpc/server';
 
 import { create } from './create';
+import { getAll } from './get-all';
 
 export const subscriptionRouter = {
+  getAll,
+
   create,
 } satisfies TRPCRouterRecord;
