bd6d6c0e11a422ce3b175d47d1aa8484c4d9e499
Alexis Guzman <codingcodax@gmail.com>
2025-03-09 17:49:33 -0600

    feat(ui): update `border-radius` to `lg`

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -3,7 +3,7 @@ import { tv } from 'tailwind-variants';
 
 const skeletonStyles = tv({
   base: clsx(
-    'transition-background pointer-events-none relative overflow-hidden rounded-md bg-grey-bg duration-300',
+    'transition-background pointer-events-none relative overflow-hidden rounded-lg bg-grey-bg duration-300',
     'before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:border-t before:border-grey-bg-active before:bg-gradient-to-r before:from-transparent before:via-grey-bg-active/80 before:to-transparent',
   ),
 });
