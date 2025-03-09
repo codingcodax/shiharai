c9b773e318499e6e3464954e49e4ed55af8dbf69
Alexis Guzman <codingcodax@gmail.com>
2025-03-09 12:40:25 -0600

    feat(actions): create `new-subscription`

--- src/app/(app)/new/_components/new-subscription-form/actions.ts
+++ src/app/(app)/new/_components/new-subscription-form/actions.ts
@@ -4,13 +4,30 @@ import { TRPCError } from '@trpc/server';
 
 import { action } from '~/app/lib/safe-action';
 import { api } from '~/trpc/server';
-import { NewPaymentMethod } from './schema';
+import { NewPaymentMethod, NewSubscription } from './schema';
 
 export const newPaymentMethod = action
   .schema(NewPaymentMethod)
   .action(async ({ parsedInput: input }) => {
     try {
       await api.paymentMethod.create(input);
+    } catch (error) {
+      if (!(error instanceof TRPCError)) throw error;
+
+      switch (error.code) {
+        case 'NOT_FOUND':
+          return { error: 'No se encontró el color.' };
+        default:
+          return { error: 'Ha ocurrido un error.' };
+      }
+    }
+  });
+
+export const newSubscription = action
+  .schema(NewSubscription)
+  .action(async ({ parsedInput: input }) => {
+    try {
+      await api.subscription.create(input);
     } catch (error) {
       if (!(error instanceof TRPCError)) throw error;
 
