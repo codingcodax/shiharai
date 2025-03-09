07fc905bcc59c38ec1260690d1f5b8bb39b9e783
Alexis Guzman <codingcodax@gmail.com>
2025-03-08 21:57:50 -0600

    feat(pages): change `new-subscription-form` import to dynamic import

--- src/app/(app)/new/page.tsx
+++ src/app/(app)/new/page.tsx
@@ -1,4 +1,21 @@
-import { NewSubscriptionForm } from './_components/new-subscription-form';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const NewSubscriptionForm = dynamic(
+  () =>
+    import('./_components/new-subscription-form').then(
+      (mod) => mod.NewSubscriptionForm,
+    ),
+  {
+    ssr: false,
+    loading: () => (
+      <div>
+        <p>here will go the skeleton</p>
+      </div>
+    ),
+  },
+);
 
 const Page = () => {
   return (
