5120bb6e6ca6ee7ccb849ce85b1bb3c986081c60
Alexis Guzman <codingcodax@gmail.com>
2025-03-09 19:15:04 -0600

    feat(pages): show user name and profile image

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,21 @@
-const Page = () => {
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { api } from '~/trpc/server';
+
+const Page = async () => {
+  const user = await api.user.getById({});
+
   return (
-    <div>
-      <p>dashboard page</p>
+    <div className='p-4'>
+      <div className='flex items-start justify-between'>
+        <div>
+          <p className='text-lg text-grey-text'>Hey,</p>
+          <h1 className='text-2xl font-bold'>{user.name?.split(' ')[0]}</h1>
+        </div>
+        <Avatar className='size-[60px] border'>
+          <AvatarImage alt={`${user.name} avatar`} src={user.image ?? ''} />
+          <AvatarFallback>JD</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 };
