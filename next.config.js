9cd985da2e80aab3cfaa693535bf952e98b824e3
Alexis Guzman <codingcodax@gmail.com>
2025-03-16 17:16:03 -0600

    feat(next): add `www.google.com` to `remote-patterns`

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const config = {
         port: '',
         pathname: '/u/*',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        port: '',
+        pathname: '/s2/*',
+      },
     ],
   },
 };
