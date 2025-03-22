605df505da896d65df34134e8a7a1c62cd099663
Alexis Guzman <codingcodax@gmail.com>
2025-03-21 20:51:02 -0600

    feat(components): use `payment-method.qty` method

--- src/app/(app)/settings/_components/payment-method-cell.tsx
+++ src/app/(app)/settings/_components/payment-method-cell.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 
 import { Cell, CellBody, CellTitle, CellValue } from '~/components/ui/cell';
+import { Skeleton } from '~/components/ui/skeleton';
+import { api } from '~/trpc/server';
 
-export const PaymentMethodCell = () => {
+export const PaymentMethodCell = async () => {
+  const paymentMethodsQty = await api.paymentMethod.qty();
+
   return (
     <Cell asChild>
       <Link href='/payment-methods'>
@@ -10,7 +14,22 @@ export const PaymentMethodCell = () => {
           <CellTitle>Payment Methods</CellTitle>
         </CellBody>
         <CellBody>
-          <CellValue>3</CellValue>
+          <CellValue>{paymentMethodsQty}</CellValue>
+        </CellBody>
+      </Link>
+    </Cell>
+  );
+};
+
+export const PaymentMethodSkeleton = () => {
+  return (
+    <Cell asChild>
+      <Link href='/payment-methods'>
+        <CellBody>
+          <CellTitle>Payment Methods</CellTitle>
+        </CellBody>
+        <CellBody>
+          <Skeleton className='my-[5px] ml-auto h-3.5 w-2' />
         </CellBody>
       </Link>
     </Cell>
