9cbf1a081481354bd93490bbdb7aa02d0da12938
Alexis Guzman <alexis12989x@gmail.com>
2025-08-14 19:35:40 -0600

    feat(auth): set `auth` config

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,10 +1,19 @@
-import NextAuth from "next-auth";
-import { cache } from "react";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { env } from "~/env";
+import { db } from "~/server/db";
 
-import { authConfig } from "./config";
-
-const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
-
-const auth = cache(uncachedAuth);
-
-export { auth, handlers, signIn, signOut };
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	socialProviders: {
+		github: {
+			clientId: env.AUTH_GITHUB_ID,
+			clientSecret: env.AUTH_GITHUB_SECRET,
+		},
+	},
+});
