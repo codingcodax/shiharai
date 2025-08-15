00909ad547ad64ab9e3c7233e6ae2a225026b60b
Alexis Guzman <alexis12989x@gmail.com>
2025-08-14 21:43:15 -0600

    feat(middleware): initial setup for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse, userAgent } from 'next/server';
+import { headers } from 'next/headers';
+import { auth } from '~/server/auth';
+
+export const middleware = async (req: NextRequest) => {
+	const { pathname } = req.nextUrl;
+	const { device } = userAgent(req);
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (device.type !== 'mobile') {
+		return NextResponse.rewrite(new URL('/unsupported-device', req.url));
+	}
+
+	if (!session) {
+		if (authPages.includes(pathname)) {
+			return NextResponse.next();
+		}
+
+		return NextResponse.redirect(new URL('/login', req.url));
+	}
+
+	if (session && authPages.includes(pathname)) {
+		return NextResponse.redirect(new URL('/dashboard', req.url));
+	}
+
+	return NextResponse.next();
+};
+
+export const config = {
+	runtime: 'nodejs',
+	matcher: ['/login/:path*', '/dashboard/:path*'],
+};
+
+const authPages = ['/login'];
