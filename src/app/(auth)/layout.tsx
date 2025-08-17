21a90899dbd5d02aa373645c274b775b8cc3084a
Alexis Guzman <alexis12989x@gmail.com>
2025-08-16 19:11:29 -0600

    feat(layout): create for `(auth)` group route

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+const Layout = ({ children }: React.PropsWithChildren) => {
+	return (
+		<div className='flex h-dvh flex-col p-4'>
+			<div className='absolute inset-0 z-10 bg-black/20' />
+
+			<div className='relative z-0 flex flex-1 items-center justify-center'>
+				<div className='relative'>
+					<div className='absolute inset-0 flex items-center justify-center'>
+						<div className='size-32 shrink-0 rounded-full border' />
+					</div>
+					<div className='absolute inset-0 flex items-center justify-center'>
+						<div className='size-40 shrink-0 rounded-full border opacity-80' />
+					</div>
+					<div className='absolute inset-0 flex items-center justify-center'>
+						<div className='size-48 shrink-0 rounded-full border opacity-60' />
+					</div>
+
+					<div className='relative z-10 size-16 rounded-lg bg-primary'></div>
+				</div>
+			</div>
+
+			{children}
+		</div>
+	);
+};
+
+export default Layout;
