6e27895eb7f9d79122cab8f3ac6f9df1deebfa74
Alexis Guzman <alexis12989x@gmail.com>
2025-08-17 12:35:42 -0600

    feat(db): create `create-enum`

--- src/server/db/schema/_table.ts
+++ src/server/db/schema/_table.ts
@@ -1,3 +1,5 @@
-import { pgTableCreator } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTableCreator } from 'drizzle-orm/pg-core';
 
 export const createTable = pgTableCreator((name) => `shiharai_${name}`);
+export const createEnum = (name: string, values: [string, ...string[]]) =>
+	pgEnum(`shiharai_${name}`, values);
