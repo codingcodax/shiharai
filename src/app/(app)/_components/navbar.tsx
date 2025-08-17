c790ca1f75d6c7cd02a3951961d1779de32f2579
Alexis Guzman <alexis12989x@gmail.com>
2025-08-17 13:13:40 -0600

    feat(components): create `navbar`

---
+++ src/app/(app)/_components/navbar.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import {
+	BellIcon,
+	HomeIcon,
+	LayoutListIcon,
+	PlusIcon,
+	SettingsIcon,
+} from 'lucide-react';
+
+import {
+	Tabbar,
+	TabbarIcon,
+	TabbarItem,
+	TabbarList,
+	TabbarName,
+	TabbarTrigger,
+} from '~/components/ui/tabbar';
+
+const pages = [
+	{
+		icon: HomeIcon,
+		name: 'Home',
+		href: '/dashboard',
+	},
+	{
+		icon: LayoutListIcon,
+		name: 'Subscriptions',
+		href: '/subscriptions',
+	},
+	{
+		icon: PlusIcon,
+		name: 'New',
+		href: '/new-subscription',
+	},
+	{
+		icon: BellIcon,
+		name: 'Notifications',
+		href: '/notifications',
+	},
+	{
+		icon: SettingsIcon,
+		name: 'Settings',
+		href: '/settings',
+	},
+];
+
+export const Navbar = () => {
+	const pathname = usePathname();
+
+	return (
+		<Tabbar>
+			<TabbarList>
+				{pages.map(({ icon: Icon, name, href }) => (
+					<TabbarItem key={name}>
+						<TabbarTrigger
+							asChild
+							data-state={pathname === href ? 'active' : ''}
+						>
+							<Link href={href}>
+								<TabbarIcon>
+									<Icon />
+								</TabbarIcon>
+								<TabbarName>{name}</TabbarName>
+							</Link>
+						</TabbarTrigger>
+					</TabbarItem>
+				))}
+			</TabbarList>
+		</Tabbar>
+	);
+};
