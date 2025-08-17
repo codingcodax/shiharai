6cbe03080086480387c1b23d6d026e9bdc9ff482
Alexis Guzman <alexis12989x@gmail.com>
2025-08-16 19:11:46 -0600

    refactor(pages): remove "duplicated" code from layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -9,38 +9,18 @@ import { GithubLogin } from './_components/github-login';
 
 const Page = () => {
 	return (
-		<div className='flex h-dvh flex-col p-4'>
-			<div className='absolute inset-0 z-10 bg-black/20' />
+		<Card className='relative z-20'>
+			<CardHeader>
+				<CardTitle className='text-2xl'>Welcome back</CardTitle>
+				<CardDescription>
+					Log in to your account to continue where you left off.
+				</CardDescription>
+			</CardHeader>
 
-			<div className='relative z-0 flex flex-1 items-center justify-center'>
-				<div className='relative'>
-					<div className='absolute inset-0 flex items-center justify-center'>
-						<div className='size-32 shrink-0 rounded-full border' />
-					</div>
-					<div className='absolute inset-0 flex items-center justify-center'>
-						<div className='size-40 shrink-0 rounded-full border opacity-80' />
-					</div>
-					<div className='absolute inset-0 flex items-center justify-center'>
-						<div className='size-48 shrink-0 rounded-full border opacity-60' />
-					</div>
-
-					<div className='relative z-10 size-16 rounded-lg bg-primary'></div>
-				</div>
-			</div>
-
-			<Card className='relative z-20'>
-				<CardHeader>
-					<CardTitle className='text-2xl'>Welcome back</CardTitle>
-					<CardDescription>
-						Log in to your account to continue where you left off.
-					</CardDescription>
-				</CardHeader>
-
-				<CardContent className='space-y-4'>
-					<GithubLogin />
-				</CardContent>
-			</Card>
-		</div>
+			<CardContent className='space-y-4'>
+				<GithubLogin />
+			</CardContent>
+		</Card>
 	);
 };
 
