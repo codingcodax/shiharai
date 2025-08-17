d751232d03aa52a07eb017cc851971278b4528cf
Alexis Guzman <alexis12989x@gmail.com>
2025-08-16 21:34:00 -0600

    chore(auth): remove `sign-in-with-github`

--- src/server/auth/client.ts
+++ src/server/auth/client.ts
@@ -3,6 +3,3 @@ import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({});
 
 export const { signIn, signOut, useSession } = authClient;
-
-export const signInWithGithub = () =>
-	signIn.social({ provider: 'github', callbackURL: '/dashboard' });
