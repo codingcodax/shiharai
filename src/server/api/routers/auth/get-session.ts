1b1cd83260710b8f3fd39c0f89f1895e0b5b1880
Alexis Guzman <alexis12989x@gmail.com>
2025-08-17 15:47:30 -0600

    feat(api): export `auth.get-session` res type

--- src/server/api/routers/auth/get-session.ts
+++ src/server/api/routers/auth/get-session.ts
@@ -1,5 +1,8 @@
+import type { RouterOutputs } from '~/trpc/react';
 import { protectedProcedure } from '../../trpc';
 
 export const getSession = protectedProcedure.query(async ({ ctx }) => {
 	return ctx.session;
 });
+
+export type GetSession = RouterOutputs['auth']['getSession'];
