b7cb653bf5c96a9ab60f230ae7d49e431e217eef
Alexis Guzman <alexis12989x@gmail.com>
2025-08-17 21:44:08 -0600

    feat(api): add `get` method

--- src/server/api/routers/user/user.ts
+++ src/server/api/routers/user/user.ts
@@ -1,6 +1,9 @@
 import { createTRPCRouter } from '../../trpc';
+import { get } from './get';
 import { update } from './update';
 
 export const userRouter = createTRPCRouter({
+	get,
+
 	update,
 });
