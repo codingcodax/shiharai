e128fda2bd3240642df9848bd3416101ec3d1a4b
Alexis Guzman <alexis12989x@gmail.com>
2025-08-17 20:14:39 -0600

    feat(api): create `user.update` method

---
+++ src/server/api/routers/user/update.ts
@@ -0,0 +1,32 @@
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+
+import { user } from '~/server/db/schema';
+import { protectedProcedure } from '../../trpc';
+
+const schema = z.object({
+	id: z.string().min(1),
+	name: z.string().optional(),
+	email: z.email().optional(),
+	emailVerified: z.boolean().optional(),
+	image: z.string().optional(),
+	timezone: z.string().optional(),
+	currency: z.string().optional(),
+});
+
+export const update = protectedProcedure
+	.input(schema)
+	.mutation(async ({ input, ctx }) => {
+		const { id, ...updateFields } = input;
+		const validUpdates = Object.fromEntries(
+			Object.entries(updateFields).filter(([_, v]) => v !== undefined),
+		);
+
+		if (Object.keys(validUpdates).length === 0) {
+			throw new Error('No fields provided to update.');
+		}
+
+		await ctx.db.update(user).set(validUpdates).where(eq(user.id, id));
+
+		return { success: true };
+	});
